b788e12edf17b1c1aa2cd62d6e2ea6eba50b70af
ajaykumar-789 <ajaykumarshanigarapu749@gmail.com>
2024-11-12 10:37:20 +0530

    Create Main.java

---
+++ eb_dsa/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class solution{
+  public static void main(string[]args){
+    Scanner sc=new Scanner(System.in);
+    int x=sc.nextInt();
+    int y=sc.nextInt();
+    sum=x+y;
+    System.out.println(sum);
+  }
+}
+      

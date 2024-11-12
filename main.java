1e0c5c2623809d13b30aca24f58231613dc7c622
ajaykumar-789 <ajaykumarshanigarapu749@gmail.com>
2024-11-12 10:30:58 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class solution {
+  public static void main(string[] args) {
+    Scanner sc=new Scanner(System.in);
+    int x=sc.nextInt();
+    int y=sc.nextIn();
+    sum=x+y;
+    System.out.println(sum);
+  }
+}
